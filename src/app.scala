591bd2ff0b37b00db32c9671671a6a5130063853
Okko Katajamaki <katajamakiokko@gmail.com>
2024-02-19 18:55:42 +0200

    Multiple updates and fixes

--- src/app.scala
+++ src/app.scala
@@ -21,10 +21,12 @@ object Main extends App:
   println("syötä teksti:")
   var userInputArray: Array[String] = Array.empty
   var inputLine = StdIn.readLine()
+  var latest = "jotain"
   while inputLine != "run" do
-     userInputArray :+= inputLine
-     inputLine = StdIn.readLine()
-  "cmd /C cls".!
+    if inputLine.trim.nonEmpty || latest.trim.nonEmpty then
+      userInputArray :+= inputLine
+    latest = inputLine
+    inputLine = StdIn.readLine()
 
   userInputArray.foreach(line =>
     if line == "" then
@@ -36,8 +38,8 @@ object Main extends App:
       fourcommon2 += Buffer.fill(12)("")
       fourcommon3 += Buffer.fill(12)("")
     else
-      val cleanedline = line.replaceAll("^\\s+", " ").replace("”", "").replace("–", "").replace("-", "").replaceAll("\\s{2,}", " ").toLowerCase
-      lines += line.replaceAll("^\\s+", "")
+      val cleanedline = line.replaceAll("-", "").replaceAll("^\\s+", "").replace("”", "").replace("–", "").replace("!", "").replaceAll("[^a-zA-Z0-9\\säöå]", "").replaceAll("\\s{2,}", " ").toLowerCase
+      lines += line.replaceAll("^\\s+", "").replaceAll("\"", "”").replaceAll("-", "—")
       fourcommon += helper.commonVowels(cleanedline)
       fourcommon2 += helper.common2(cleanedline)
       fourcommon3 += helper.commonVowelPairs(cleanedline)
@@ -49,7 +51,9 @@ object Main extends App:
 
         )
       val weak = helper.shorten(starters)
-      if helper.countDuplicates(starters).values.filter(_ > 1).toBuffer.nonEmpty then
+      val nice = helper.countDuplicates(starters).values.filter(_ > 1).toBuffer
+
+      if nice.nonEmpty then
          cat += "S"
          scount += helper.countDuplicates(starters).values.filter(_ > 1).toBuffer.mkString("+")
 
@@ -97,22 +101,24 @@ object Main extends App:
 
 
     println("Säkeet kategorioittain. Säkeitä yhteensä " + cat.length + " kappaletta" )
-    println("S: " + cat.count(_ == "S") + " (" + (BigDecimal(cat.count(_ == "S").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP).toString() + ")")
-    println("W: " + cat.count(_ == "W") + " (" + (BigDecimal(cat.count(_ == "W").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP).toString() + ")")
-    println("N: " + cat.count(_ == "N") + " (" + (BigDecimal(cat.count(_ == "N").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP).toString() + ")")
+
+    println("S: " + cat.count(_ == "S") + " (" + FINN.format((BigDecimal(cat.count(_ == "S").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP)) + ")")
+    println("W: " + cat.count(_ == "W") + " (" + FINN.format((BigDecimal(cat.count(_ == "W").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP)) + ")")
+    println("N: " + cat.count(_ == "N") + " (" + FINN.format((BigDecimal(cat.count(_ == "N").toDouble / cat.length) * 100).setScale(3, BigDecimal.RoundingMode.HALF_UP)) + ")")
 
 
-    printf("%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\n", "Line", "Cat", "3CAT", "S-count", "W-count", "V1", "V1C", "V1%", "V2", "V2C", "V2%", "V3", "V3C", "V3%", "V4", "V4C", "V4%", "V1", "V1C", "V1%", "V2", "V2C", "V2%", "V3", "V3C", "V3%", "V4", "V4C", "V4%")
+    printf("%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\t%-1s\n", "Line", "Cat", "3CAT", "S-count", "W-count", "V1", "V1C", "V1%", "V2", "V2C", "V2%", "V3", "V3C", "V3%", "V4", "V4C", "V4%", "K1", "K1C", "K1%", "K2", "K2C", "K2%", "K3", "K3C", "K3%", "K4", "K4C", "K4%", "D1", "D1C", "D1%", "D2", "D2C", "D2%", "D3", "D3C", "D3%", "D4", "D4C", "D4%")
 
     for (i <- lines.indices) do
       val cleanedLine = lines(i).replaceAll("[\n\r]", "")
       val vokaalit = fourcommon(i).padTo(12, "")
       val vokaalit2 = fourcommon2(i).padTo(12, "")
-      val vokaalit3 = fourcommon2(i).padTo(12, "")
-      printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+      val vokaalit3 = fourcommon3(i).padTo(12, "")
+      printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
            cleanedLine, cat(i), catsliding(i), scount(i), wcount(i), vokaalit(0), vokaalit(1), vokaalit(2), vokaalit(3), vokaalit(4),
            vokaalit(5), vokaalit(6), vokaalit(7), vokaalit(8), vokaalit(9), vokaalit(10), vokaalit(11), vokaalit2(0), vokaalit2(1), vokaalit2(2), vokaalit2(3), vokaalit2(4),
-           vokaalit2(5), vokaalit2(6), vokaalit2(7), vokaalit2(8), vokaalit2(9), vokaalit2(10), vokaalit2(11))
+           vokaalit2(5), vokaalit2(6), vokaalit2(7), vokaalit2(8), vokaalit2(9), vokaalit2(10), vokaalit2(11),vokaalit3(0), vokaalit3(1), vokaalit3(2), vokaalit3(3), vokaalit3(4),
+           vokaalit3(5), vokaalit3(6), vokaalit3(7), vokaalit3(8), vokaalit3(9), vokaalit3(10), vokaalit3(11))
 
 
 
@@ -144,6 +150,22 @@ object helper:
     result
 
 
+  def replaceCharsFromString(input: String, replacements: String): String =
+    var output = input
+    val charCounts = replacements.groupMapReduce(identity)(_ => 1)(_ + _)
+
+    for (charToReplace, count) <- charCounts do
+      var remainingCount = count
+      var index = output.indexOf(charToReplace)
+
+      while index != -1 && remainingCount > 0 do
+        output = output.substring(0, index) + output.substring(index + 1)
+        remainingCount -= 1
+        index = output.indexOf(charToReplace, index)
+
+    output
+
+
   def isVowel(ch: Char): Boolean =
     ch.toLower match
       case 'a' | 'e' | 'i' | 'o' | 'u' | 'y' | 'ä' | 'ö' | 'å' => true
@@ -154,7 +176,6 @@ object helper:
       case 'b' | 'c' | 'd' | 'f' | 'g' | 'h' | 'j' | 'k' | 'l' | 'm' |
            'n' | 'p' | 'q' | 'r' | 's' | 't' | 'v' | 'w' | 'x' | 'z' => true
       case _ => false
-
 
   def commonVowels(line:  String) =
     val finnishLocale = new Locale("fi", "FI") // Finnish locale
@@ -210,7 +231,7 @@ object helper:
     // Define a function to check if a pair is a vowel pair
     def isVowelPair(pair: String): Boolean =
       val vowels = "aeiouyäö"
-      pair.length == 2 && pair.forall(vowels.contains)
+      pair.length == 2 && pair.forall(vowels.contains) && pair(0) != pair(1)
 
     // Iterate through the line by pairs
     line.sliding(2).foreach(pair =>
